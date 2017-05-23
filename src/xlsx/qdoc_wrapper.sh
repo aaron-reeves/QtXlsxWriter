a2e9f6579c40209d0a458cfa0cb42c66dcd3c583
Aaron Reeves <aaron.reeves@sruc.ac.uk>
2017-05-23 14:23:42 +0100

    Updated build files for Qt 5.8

--- src/xlsx/qdoc_wrapper.sh
+++ src/xlsx/qdoc_wrapper.sh
@@ -5,6 +5,8 @@ QT_VER=0.3
 export QT_VER
 QT_VERSION_TAG=030
 export QT_VERSION_TAG
-QT_INSTALL_DOCS=C:/Qt/Docs/Qt-5.7
+QT_INSTALL_DOCS=C:/Qt/Docs/Qt-5.8
 export QT_INSTALL_DOCS
-exec 'C:\Qt\5.7\mingw53_32\bin\qdoc.exe' "$@"
+BUILDDIR=C:/Users/areeves/Documents/ResearchProjects/Tools/QtXlsxWriter/src/xlsx
+export BUILDDIR
+exec 'C:\Qt\5.8\mingw53_32\bin\qdoc.exe' "$@"
