22c7e1da1df34e7239a9efc9f5bcd6d1c2a41184
Golubchikov Mihail <golubchikov.mihail@gmail.com>
2017-11-23 11:41:51 +0300

    Add field _sheet to CellReference

--- src/xlsx/xlsxcellreference.h
+++ src/xlsx/xlsxcellreference.h
@@ -24,7 +24,10 @@
 ****************************************************************************/
 #ifndef QXLSX_XLSXCELLREFERENCE_H
 #define QXLSX_XLSXCELLREFERENCE_H
+
 #include "xlsxglobal.h"
+
+#include <QString>
 
 QT_BEGIN_NAMESPACE_XLSX
 
@@ -32,20 +35,22 @@ class Q_XLSX_EXPORT CellReference
 {
 public:
     CellReference();
-    CellReference(int row, int column);
+    CellReference(int row, int column, const QString &sheet = QString());
     CellReference(const QString &cell);
     CellReference(const char *cell);
     CellReference(const CellReference &other);
     ~CellReference();
 
     QString toString(bool row_abs=false, bool col_abs=false) const;
-    static QString toString(int row, int column);
+    static QString toString(int row, int column, const QString &sheet = QString());
     static CellReference fromString(const QString &cell);
     bool isValid() const;
     inline void setRow(int row) { _row = row; }
     inline void setColumn(int col) { _column = col; }
+    inline void setSheet(const QString &sheet) { _sheet = sheet; }
     inline int row() const { return _row; }
     inline int column() const { return _column; }
+    inline const QString &sheet() const { return _sheet; }
 
     inline bool operator ==(const CellReference &other) const
     {
@@ -58,6 +63,7 @@ public:
 private:
     void init(const QString &cell);
     int _row, _column;
+    QString _sheet;
 };
 
 QT_END_NAMESPACE_XLSX
