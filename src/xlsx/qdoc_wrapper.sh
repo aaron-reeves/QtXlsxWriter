beeb28c5bc2e48eb4d8ddbf2fdfb9eb157da62cb
Aaron Reeves <aaron.reeves@sruc.ac.uk>
2019-06-08 22:41:10 +0100

    Updated for building with Qt 5.10

--- src/xlsx/qdoc_wrapper.sh
+++ src/xlsx/qdoc_wrapper.sh
@@ -5,8 +5,8 @@ QT_VER=0.3
 export QT_VER
 QT_VERSION_TAG=030
 export QT_VERSION_TAG
-QT_INSTALL_DOCS=C:/Qt/Docs/Qt-5.8
+QT_INSTALL_DOCS=C:/Qt/Docs/Qt-5.10.0
 export QT_INSTALL_DOCS
-BUILDDIR=C:/Users/areeves/Documents/ResearchProjects/Tools/QtXlsxWriter/src/xlsx
+BUILDDIR=C:/Users/areeves/Documents/_A_ResearchProjects/Tools/QtXlsxWriter/src/xlsx
 export BUILDDIR
-exec 'C:\Qt\5.8\mingw53_32\bin\qdoc.exe' "$@"
+exec 'C:\Qt\5.10.0\mingw53_32\bin\qdoc.exe' "$@"
